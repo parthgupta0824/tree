9ca9a84be401a490deeb34114170723814e02872
Parth Gupta <95039857+par-thgupta@users.noreply.github.com>
2023-04-18 11:10:32 +0530

    master

--- 543. Diameter of Binary Tree.cpp
+++ 543. Diameter of Binary Tree.cpp
@@ -9,8 +9,8 @@ public:
         return max(l,r)+1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int l = maxlen(root->left);
         int r = maxlen(root->right);
+        int l = maxlen(root->left);
         fans = max(fans,(l+r));
         return fans;
     }
